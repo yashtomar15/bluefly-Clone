6238ccda517bc143e38fb7b38e36758fa8c18e3d
Prakash Gupta <96722484+prakash817@users.noreply.github.com>
2022-06-17 09:23:47 +0530

    app.js file

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./Component/Footer";
+import NewArrival from "./Component/NewArrival";
+import Offer from "./Component/Offer";
+import ShopyCategory from "./Component/ShopyCategory";
+import Tab from "./Component/Tab";
+import Trending from "./Component/Trending";
+
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [scrWidth, setScrWidth] = useState(1200);
+  useEffect(() => {
+    setScrWidth(window.innerWidth);
+  }, [window.innerWidth]);
+
+  return (
+    <div className="App">
+      <ShopyCategory />
+
+      <NewArrival />
+
+      <Trending />
+
+      <Offer />
+
+      {scrWidth <= 700 ? <Tab /> : <Footer />}
+    </div>
+  );
 }
 
 export default App;
