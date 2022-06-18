5daf2faa2bdd692d95e02039082a6ae28ff637f8
Aniket Solanki <solankianiket0411@gmail.com>
2022-06-18 19:30:44 +0530

    Update index.js

--- Backend/Data/userdata.js
+++ Backend/Data/userdata.js
@@ -5,30 +5,22 @@ const bluesky = require("../Mongo/dbbody");
 const Userdata = Router();
 
 //http://localhost:8080/Data/
-<<<<<<< HEAD
 Userdata.get("/", async (req, res) => {
   const data = await bluesky.find();
   console.log(data);
   res.send(data);
 });
 
-=======
-Userdata.get("/",async(req,res)=>{
-
-    const data=await bluesky.find()
-    console.log(data)
-    res.send(data)
-})
-
-Userdata.get("/Men's",async(req,res)=>{
-    const data1=await bluesky.find({category:{$regex:"Men's"}})
-    res.send(data1)
-})
-Userdata.get("/Women's",async(req,res)=>{
-    const data1=await bluesky.find({category:{$regex:"Women's",$options:"i"}})
-    res.send(data1)
-})
->>>>>>> main
+Userdata.get("/Men's", async (req, res) => {
+  const data1 = await bluesky.find({ category: { $regex: "Men's" } });
+  res.send(data1);
+});
+Userdata.get("/Women's", async (req, res) => {
+  const data1 = await bluesky.find({
+    category: { $regex: "Women's", $options: "i" },
+  });
+  res.send(data1);
+});
 //for searching
 Userdata.get("/search", async (req, res) => {
   //http://localhost:8080/Data/search?q=value;
@@ -45,7 +37,6 @@ Userdata.get("/search", async (req, res) => {
 Userdata.get("/filter", async (req, res) => {
   console.log(req.query);
 
-<<<<<<< HEAD
   //http://localhost:8080/Data/filter?category=Women's wear|| Women's jeans || Men's Pant ||Men's Shirt ;
   if (req.query.category) {
     let data1 = await bluesky.find({ category: req.query.category });
@@ -72,6 +63,12 @@ Userdata.get("/filter", async (req, res) => {
     console.log(data1);
     return res.send(data1);
   }
+  //http://localhost:8080/Data/filter?brand=polo||canvas||nike
+  else if (req.query.brand) {
+    let data1 = await bluesky.find({ brand: req.query.brand });
+    console.log(data1);
+    return res.send(data1);
+  }
   //http://localhost:8080/Data/filter?minPrice=500&maxPrice=1000
   else if (req.query.minPrice && req.query.maxPrice) {
     let data1 = await bluesky.find({
@@ -82,112 +79,81 @@ Userdata.get("/filter", async (req, res) => {
     return res.send(data1);
   }
 });
-=======
-    //http://localhost:8080/Data/filter?category=Women's wear|| Women's jeans || Men's Pant ||Men's Shirt ;
-    if(req.query.category)
-    {
-        let data1=await bluesky.find({category:req.query.category})
-        console.log(data1)
-        return res.send(data1)
-    }
-    //http://localhost:8080/Data/filter?color=white|| red|| green ||grey ;
-    else if(req.query.color)
-    {
-        let data1=await bluesky.find({$or:[{color1:req.query.color},{color2:req.query.color}]})
-        console.log(data1)
-        return res.send(data1)
-    }
-    //http://localhost:8080/Data/filter?size=small|| medium|| large
-    else if(req.query.size)
-    {
-        let data1=await bluesky.find({sizes:req.query.size})
-        console.log(data1)
-        return res.send(data1)
-    }
-     //http://localhost:8080/Data/filter?condition=new||used
-    else if(req.query.condition)
-    {
-        let data1=await bluesky.find({condition:req.query.condition})
-        console.log(data1)
-        return res.send(data1)
-    }
-     //http://localhost:8080/Data/filter?brand=polo||canvas||nike
-     else if(req.query.brand)
-     {
-         let data1=await bluesky.find({brand:req.query.brand})
-         console.log(data1)
-         return res.send(data1)
-     }
-    //http://localhost:8080/Data/filter?minPrice=500&maxPrice=1000
-    else if(req.query.minPrice && req.query.maxPrice)
-    {
-        let data1=await bluesky.find({price:{$gt:req.query.minPrice},price:{$lt:req.query.maxPrice}})
-        console.log(data1)
-        return res.send(data1)
-    }
-})
->>>>>>> main
 
 // New contribution by yash---->
 // For sorting women's data
 
-Userdata.get("/sortwomens",async (req,res)=>{    
-const {title,price}=req.query;
+Userdata.get("/sortwomens", async (req, res) => {
+  const { title, price } = req.query;
 
-{
-//http://localhost:8080/Data/sort?title=inc
-if(title&& title==='inc'){
-    let data=await bluesky.find({category:{$regex:"women's", $options:"i"}}).sort({title:1});
-    res.send(data);
-}
-//http://localhost:8080/Data/sort?title=dec
-else if(title&& title==='dec'){
-    let data=await bluesky.find({category:{$regex:"women's", $options:"i"}}).sort({title:-1});
-    res.send(data);
-}
-//http://localhost:8080/Data/sort?price=inc
-else if(price && price==='inc'){
-    let data=await bluesky.find({category:{$regex:"women's", $options:"i"}}).sort({price:1});
-    res.send(data);
-}
-//http://localhost:8080/Data/sort?price=dec
-else if(price && price==='dec'){
-    let data=await bluesky.find({category:{$regex:"women's", $options:"i"}}).sort({price:-1});
-    res.send(data);
-}
-
-}
-})
+  {
+    //http://localhost:8080/Data/sort?title=inc
+    if (title && title === "inc") {
+      let data = await bluesky
+        .find({ category: { $regex: "women's", $options: "i" } })
+        .sort({ title: 1 });
+      res.send(data);
+    }
+    //http://localhost:8080/Data/sort?title=dec
+    else if (title && title === "dec") {
+      let data = await bluesky
+        .find({ category: { $regex: "women's", $options: "i" } })
+        .sort({ title: -1 });
+      res.send(data);
+    }
+    //http://localhost:8080/Data/sort?price=inc
+    else if (price && price === "inc") {
+      let data = await bluesky
+        .find({ category: { $regex: "women's", $options: "i" } })
+        .sort({ price: 1 });
+      res.send(data);
+    }
+    //http://localhost:8080/Data/sort?price=dec
+    else if (price && price === "dec") {
+      let data = await bluesky
+        .find({ category: { $regex: "women's", $options: "i" } })
+        .sort({ price: -1 });
+      res.send(data);
+    }
+  }
+});
 
 // For sorting mens data
 
-Userdata.get("/sortmens",async (req,res)=>{    
-    const {title,price}=req.query;
-    
-    {
+Userdata.get("/sortmens", async (req, res) => {
+  const { title, price } = req.query;
+
+  {
     //http://localhost:8080/Data/sortmens?title=inc
-    if(title&& title==='inc'){
-        let data=await bluesky.find({category:{$regex:"men's", $options:"i"}}).sort({title:1});
-        res.send(data);
+    if (title && title === "inc") {
+      let data = await bluesky
+        .find({ category: { $regex: "men's", $options: "i" } })
+        .sort({ title: 1 });
+      res.send(data);
     }
     //http://localhost:8080/Data/sortmens?title=dec
-    else if(title&& title==='dec'){
-        let data=await bluesky.find({category:{$regex:"men's", $options:"i"}}).sort({title:-1});
-        res.send(data);
+    else if (title && title === "dec") {
+      let data = await bluesky
+        .find({ category: { $regex: "men's", $options: "i" } })
+        .sort({ title: -1 });
+      res.send(data);
     }
     //http://localhost:8080/Data/sortmens?price=inc
-    else if(price && price==='inc'){
-        let data=await bluesky.find({category:{$regex:"men's", $options:"i"}}).sort({price:1});
-        res.send(data);
+    else if (price && price === "inc") {
+      let data = await bluesky
+        .find({ category: { $regex: "men's", $options: "i" } })
+        .sort({ price: 1 });
+      res.send(data);
     }
     //http://localhost:8080/Data/sortmens?price=dec
-    else if(price && price==='dec'){
-        let data=await bluesky.find({category:{$regex:"men's", $options:"i"}}).sort({price:-1});
-        res.send(data);
+    else if (price && price === "dec") {
+      let data = await bluesky
+        .find({ category: { $regex: "men's", $options: "i" } })
+        .sort({ price: -1 });
+      res.send(data);
     }
-    
-    }
-    })
+  }
+});
 
 //for getting particlar data like types of cateogary or different colors or brand or sizes
 
@@ -239,7 +205,6 @@ Userdata.get("/myval", async (req, res) => {
   if (req.query.condition) {
     let brr = [];
 
-<<<<<<< HEAD
     data.map((ele) => {
       if (!brr.includes(ele.condition)) {
         brr.push(ele.condition);
@@ -248,32 +213,18 @@ Userdata.get("/myval", async (req, res) => {
     console.log(brr);
     res.send(brr);
   }
-});
-=======
-        data.map((ele)=>{
-            if(!brr.includes(ele.condition))
-            {
-                brr.push(ele.condition)
-            }
-        })
-        console.log(brr)
-        res.send(brr)
-    }
-    //http://localhost:8080/Data/myval?brands=find
-    if(req.query.brands)
-    {
-        let brr=[]
+  //http://localhost:8080/Data/myval?brands=find
+  if (req.query.brands) {
+    let brr = [];
 
-        data.map((ele)=>{
-            if(!brr.includes(ele.brand))
-            {
-                brr.push(ele.brand)
-            }
-        })
-        console.log(brr)
-        res.send(brr)
-    }
-})
->>>>>>> main
+    data.map((ele) => {
+      if (!brr.includes(ele.brand)) {
+        brr.push(ele.brand);
+      }
+    });
+    console.log(brr);
+    res.send(brr);
+  }
+});
 
 module.exports = Userdata;
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 const express = require("express");
 const App = express();
-=======
-const express=require("express")
-const App=express()
-const cors=require("cors")
->>>>>>> main
+const cors = require("cors");
 
 const Userauth = require("./Auth/userauth");
 const Userdata = require("./Data/userdata");
@@ -13,39 +8,11 @@ const userpurchasing = require("./Purchase/userpurchase");
 
 const connect = require("./Mongo/dbAuth");
 
-<<<<<<< HEAD
+const { connect } = require("./Mongo/dbAuth");
+
+App.use(cors());
 App.use(express.urlencoded({ extended: true }));
 App.use(express.json());
-=======
-const {connect}=require("./Mongo/dbAuth")
-
-App.use(cors())
-App.use(express.urlencoded({extended:true}));
-App.use(express.json())
-
-App.use("/Auth",Userauth)
-App.use("/Data",Userdata)
-App.use("/Purchase",userpurchasing)
-
-App.get("/",(req,res)=>{
-    res.send("welcome to bluefly")
-})
-
-
-const port =process.env.PORT || 8080
-
-
-App.listen(port,async()=>{
-    try{
-        await connect
-        console.log(`server started at ${port} and database connected`)
-    }
-    catch{
-        console.log("something went wrong")
-    }
-
-})
->>>>>>> main
 
 App.use("/Auth", Userauth);
 App.use("/Data", Userdata);
